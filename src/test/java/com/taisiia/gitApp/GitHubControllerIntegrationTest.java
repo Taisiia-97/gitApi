e7cb4ee32112db1b9ee99a448e05b87f1e6855f7
Taisiia <taechka97@gmail.com>
2025-07-27 19:04:21 +0200

    added some changes.

--- src/test/java/com/taisiia/gitApp/GitHubControllerIntegrationTest.java
+++ src/test/java/com/taisiia/gitApp/GitHubControllerIntegrationTest.java
@@ -6,26 +6,51 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.not;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock(@ConfigureWireMock(port = 8089))
+@ActiveProfiles("test")
 public class GitHubControllerIntegrationTest {
+
 
     @LocalServerPort
     private int port;
 
     @BeforeEach
-    void setup() {
+    void setup() throws IOException {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
+
+        String reposJson = Files.readString(Path.of("src/test/resources/__files/repos-response.json"));
+        String branchesJson = Files.readString(Path.of("src/test/resources/__files/branches-response.json"));
+
+        stubFor(get(urlPathMatching("/users/[^/]+/repos"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(reposJson)));
+
+        stubFor(get(urlPathMatching("/repos/[^/]+/[^/]+/branches"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(branchesJson)));
     }
 
 
     @Test
     void shouldReturnRepositoriesWithBranchesForValidUser() {
         // given
-        String userName = "Taisiia-97";
+        String userName = "octocat";
 
         // when + then
         RestAssured
